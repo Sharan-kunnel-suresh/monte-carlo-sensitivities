9634bdb7c28d5ec40022444144957146f847c9ce
Sharan Kunnel Suresh <sharan.kunnel-suresh@edu.rtu.lv>
2026-08-25 11:59:14 +0300

    Merge remote main into local main

---
+++ src/mc_sensitivities/black_scholes.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.stats import norm
+
+
+def black_scholes_call(S, K, r, sigma, T):
+    """
+    Black-Scholes price of a European call option.
+    """
+
+    d1 = (
+        np.log(S / K)
+        + (r + 0.5 * sigma**2) * T
+    ) / (sigma * np.sqrt(T))
+
+    d2 = d1 - sigma * np.sqrt(T)
+
+    return (
+        S * norm.cdf(d1)
+        - K * np.exp(-r * T) * norm.cdf(d2)
+    )
+def black_scholes_put(S, K, r, sigma, T):
+    """
+    Black-Scholes price of a European put option.
+    """
+
+    d1 = (
+        np.log(S / K)
+        + (r + 0.5 * sigma**2) * T
+    ) / (sigma * np.sqrt(T))
+
+    d2 = d1 - sigma * np.sqrt(T)
+
+    return (
+        K * np.exp(-r * T) * norm.cdf(-d2)
+        - S * norm.cdf(-d1)
+    )
